3196046e73bff01a735dc392a69fb6d654f1c01c
gpsoft <etoh.gpsoft@gmail.com>
2019-11-22 00:36:04 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# 目的
+
+ClojureScriptの練習。
+
+- 地図サービス(leaflet)
+- re-frame
+- フォーム(re-com)
+- ファイルのアップロード
+- ファイルのダウンロード
---
+++ externs.js
@@ -0,0 +1,10 @@
+var L = {
+	"marker": function(){},
+	"map": {
+		"setView": function(){},
+	},
+	"tileLayer": {
+		"addTo": function(){},
+		"bindPopup": function(){}
+	}
+};
